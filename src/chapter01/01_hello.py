4a4f69642f9783ac8b042b21d00b03cb2cf291e0
Reny Alvarado <renyalvarado@gmail.com>
2020-07-12 21:33:15 -0300

    Add exercises for Chapter 01 (Python Basics)

---
+++ src/chapter01/01_hello.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# This program says hello and asks for my name.
+
+print("Hello World")
+
+my_name = input("What is your name? ")
+print(f"It is good to meet you {my_name}")
+print(f"The length of your name is: {len(my_name)}")
+
+my_age = input("What is your age? ")
+print(f"You will be {int(my_age) + 1} in a year.")
